1e60f90e54c5075e65fc6a90136ea7879780b205
alex-waiganjo <alexwaiganjo934@gmauil.com>
2023-09-02 23:06:20 +0300

    es lint

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const items = ["C is fun","Python is cool","JavaScript is amazing"]
+const items = ["C is fun", "Python is cool", "JavaScript is amazing"]
 
 
 // Iterating through the array items
-for(let i=0;i<items.length;i++){
+for (let i = 0; i < items.length; i++) {
     console.log(items[i]);
 }
