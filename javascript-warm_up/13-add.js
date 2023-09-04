158e598af7f5c95dd63ae845989ab30a7608b644
alex-waiganjo <alexwaiganjo934@gmauil.com>
2023-09-04 14:24:30 +0300

    sum

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
 
-const add = require('./13-add').add;
-console.log(add(3, 5));
-
 // function to add two integers
-// function add(num1,num2){
-//     return num1+num2
-// }
-// console.log(add(7,9));
+function add(num1, num2) {
+  return num1 + num2;
+}
--- javascript-warm_up/main_0.js
+++ javascript-warm_up/main_0.js
@@ -2,3 +2,4 @@
 const myVar = 40;
 console.log(myVar);
 require('./100-let_me_const');
+// console.log(myVar);
