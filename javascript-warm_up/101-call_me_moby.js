cdd5b21789854a40f1b41473ad0cebde193f1df7
alex-waiganjo <alexwaiganjo934@gmauil.com>
2023-09-04 15:08:31 +0300

    task 102

--- javascript-warm_up/101-call_me_moby.js
+++ javascript-warm_up/101-call_me_moby.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
-// function callMeMoby (x, theFunction){
-
-
-
 function callMeMoby(x, theFunction) {
   if (x > 0) {
     theFunction();
@@ -12,7 +8,3 @@ function callMeMoby(x, theFunction) {
 
 module.exports.callMeMoby = callMeMoby;
 
-
-// callMeMoby(4, function () {
-//   console.log('C is fun');
-// });
---
+++ javascript-warm_up/102-add_me_maybe.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+function addMeMaybe(number, theFunction) {
+  number++;
+  theFunction(number);
+}
+
+module.exports.addMeMaybe = addMeMaybe;
+
+
