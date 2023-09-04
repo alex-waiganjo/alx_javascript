fb68d9500841ecc1f600d84814ccd3ed4decb42d
alex-waiganjo <alexwaiganjo934@gmauil.com>
2023-09-04 15:48:11 +0300

    task 103

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,22 +1,22 @@
 #!/usr/bin/node
 const myObject = {
   type: 'object',
-  value: 12
+  value: 12,
 };
 // Before adding another attribute
 console.log(myObject);
 
-// Adding incr func to myObject 
-function incr(){
-    this.value++;
-}
-// Assigning incr attribute to the incr function
-myObject.incr =incr
+// Adding incr func to myObject
+myObject.incr = function () {
+  this.value++;
+};
 
 // Output
 myObject.incr();
 console.log(myObject);
+
 myObject.incr();
 console.log(myObject);
+
 myObject.incr();
 console.log(myObject);
