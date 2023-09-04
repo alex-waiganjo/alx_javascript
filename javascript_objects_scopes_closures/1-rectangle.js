88055ee645c0f3807d1cf91489d9908b0633fa14
alex-waiganjo <alexwaiganjo934@gmauil.com>
2023-09-04 17:13:27 +0300

    error

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-// const Rectangle = require('./1-rectangle');
+// const Rectangle = require('./1-main');
+
 class Rectangle {
     constructor(width, height) {
       this.width = width;
       this.height = height;
     }
   }
-
 const r1 = new Rectangle(3, 3);
 console.log(r1);
 console.log(r1.width);
