a66556dd06aad171235087e82cbb42ce260b5745
alex-waiganjo <alexwaiganjo934@gmauil.com>
2023-09-04 16:59:36 +0300

    fill in the values

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -7,17 +7,17 @@ class Rectangle {
     }
   }
 
-const r1 = new Rectangle(2, 3);
-console.log(r1);
-console.log(r1.width);
-console.log(r1.height);
+// const r1 = new Rectangle(2, 3);
+// console.log(r1);
+// console.log(r1.width);
+// console.log(r1.height);
 
-const r2 = new Rectangle(2, -3);
-console.log(r2);
-console.log(r2.width);
-console.log(r2.height);
+// const r2 = new Rectangle(2, -3);
+// console.log(r2);
+// console.log(r2.width);
+// console.log(r2.height);
 
-const r3 = new Rectangle(2);
-console.log(r3);
-console.log(r3.width);
-console.log(r3.height);
+// const r3 = new Rectangle(2);
+// console.log(r3);
+// console.log(r3.width);
+// console.log(r3.height);
