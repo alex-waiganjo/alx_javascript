cad37adb15d7e3977fec0d5f256e64ae33f58c3c
alex-waiganjo <alexwaiganjo934@gmauil.com>
2023-09-04 17:02:28 +0300

    r1

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -7,10 +7,10 @@ class Rectangle {
     }
   }
 
-// const r1 = new Rectangle(2, 3);
-// console.log(r1);
-// console.log(r1.width);
-// console.log(r1.height);
+const r1 = new Rectangle(3, 3);
+console.log(r1);
+console.log(r1.width);
+console.log(r1.height);
 
 // const r2 = new Rectangle(2, -3);
 // console.log(r2);
