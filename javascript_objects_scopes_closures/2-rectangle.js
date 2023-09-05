869df3c98931bc6271e38b22b72978b6012209ec
alex-waiganjo <alexwaiganjo934@gmauil.com>
2023-09-05 20:13:00 +0300

    2

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// Create an empty object if w or h is equal to 0 or not a positive integer
+class Rectangle {
+    constructor(width, height) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+if ((Rectangle.width && Rectangle.height ==0 )& (Rectangle.width && Rectangle.height<0)){
+Rectangle = {}
+}
+
+module.exports = Rectangle;
