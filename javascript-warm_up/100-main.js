5949619060817534a24ea79ebc2d441194b73763
alex-waiganjo <alexwaiganjo934@gmauil.com>
2023-09-05 19:57:23 +0300

    rectangle

--- javascript-warm_up/100-main.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/node
-
-
-// require('./100-let_me_const');
--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 //class Rectangle with width and height attributes
 
-exports.Rectangle =class Rectangle {
+exports.Rectangle =class{
     constructor(width, height) {
       this.width = width;
       this.height = height;
