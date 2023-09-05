54c580047ba17b218eac10908b8b7ed079f69bbf
alex-waiganjo <alexwaiganjo934@gmauil.com>
2023-09-05 21:30:18 +0300

    103 increment object

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,38 +1,17 @@
 #!/usr/bin/node
-// const myObject = {
-//   type: 'object',
-//   value: 12,
-// };
-// // Before adding another attribute
-// console.log(myObject);
-
-// // Adding incr func to myObject
-// myObject.incr = function () {
-//   this.value++;
-// };
-
-// // Output
-// myObject.incr();
-// console.log(myObject);
-
-// // myObject.incr();
-// // console.log(myObject);
-
-// // myObject.incr();
-// // console.log(myObject);
-
-
-// Increment Object.
-
 const myObject = {
-    type: 'object',
-    value: 12
+  type: 'object',
+  value: 12,
 };
+// Before adding another attribute
 console.log(myObject);
+
+// Adding incr func to myObject
 myObject.incr = function () {
-    this.value++;
+  this.value++;
 };
 
+// Output
 myObject.incr();
 console.log(myObject);
 
