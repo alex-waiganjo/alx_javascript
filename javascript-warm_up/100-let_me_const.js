793f0240aa5452fb1321807b740b4a23b04cce04
alex-waiganjo <alexwaiganjo934@gmauil.com>
2023-09-05 18:57:08 +0300

    exporting files

--- javascript-warm_up/100-let_me_const.js
+++ javascript-warm_up/100-let_me_const.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
-const my_Var = 40;
-let myVar =333;
+const myVar = 333;
 
-// require('./100-let_me_const').myVar
-console.log(my_Var);
-console.log(myVar);
+module.exports = myVar
 
--- javascript-warm_up/100-main.js
+++ javascript-warm_up/100-main.js
@@ -1,4 +1,3 @@
-// #!/usr/bin/node
-// let myVar = 333;
-// require('./100-let_me_const').myVar
-// console.log(myVar);
+#!/usr/bin/node
+myVar = require('./100-let_me_const');
+console.log(myVar);
