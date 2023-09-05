f8322c2038b3502fc63456cb4b9a2ad835467c63
alex-waiganjo <alexwaiganjo934@gmauil.com>
2023-09-05 19:55:05 +0300

    Rectangle

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 //class Rectangle with width and height attributes
 
-class Rectangle {
+exports.Rectangle =class Rectangle {
     constructor(width, height) {
       this.width = width;
       this.height = height;
