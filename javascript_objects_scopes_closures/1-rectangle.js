ffefc1d96c6970858ecbef70008916535259fdb8
alex-waiganjo <alexwaiganjo934@gmauil.com>
2023-09-05 19:49:42 +0300

    rectangle

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// const Rectangle = require('./1-main');
+//class Rectangle with width and height attributes
 
 class Rectangle {
     constructor(width, height) {
@@ -7,17 +7,3 @@ class Rectangle {
       this.height = height;
     }
   }
-const r1 = new Rectangle(3, 3);
-console.log(r1);
-console.log(r1.width);
-console.log(r1.height);
-
-// const r2 = new Rectangle(2, -3);
-// console.log(r2);
-// console.log(r2.width);
-// console.log(r2.height);
-
-// const r3 = new Rectangle(2);
-// console.log(r3);
-// console.log(r3.width);
-// console.log(r3.height);
