48e44beb82c49be874d9b59e0b242480017649b5
alex-waiganjo <alexwaiganjo934@gmauil.com>
2023-09-07 23:26:02 +0300

    5 and 6 square

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,5 +1,5 @@
 #!/usr/bin/Node
-module.exports = class Square extends require('./5-rectangle.js') {
+module.exports = class Square extends require('./4-rectangle.js') {
     constructor (size) {
         super(size, size);
     }
--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,11 +1,12 @@
 #!/usr/bin/Node
-module.exports = class Square extends require('./5-square.js') {
-    charPrint (c) {
-        if (c === undefined) {
-            this.print();
-        }
-        else {
-            for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
-        }
+
+module.exports = class Square extends require('./5-square') {
+  charPrint(c) {
+    if (c == null) {
+      c = 'X';
     }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
 };
