84fa24a328e99fbdecfe50c80cd87c9fefba02a2
alex-waiganjo <alexwaiganjo934@gmauil.com>
2023-09-14 00:02:43 +0300

    stataus code modification

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -4,5 +4,5 @@ const req = require('request');
 // Input
 const url = process.argv[2];
 req.get(url).on('response', (response) => {
-  console.log({ code: response.statusCode });
+  console.log('code:', response.statusCode);
 });
