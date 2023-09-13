88c1fac33c9df45d84fba11b954c602da9920b01
alex-waiganjo <alexwaiganjo934@gmauil.com>
2023-09-14 01:10:13 +0300

    file path changes

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 const req = require('request')
 const url = process.argv[2]
+const file_path =process.argv[3]
 const fs= require('fs')
 
-req.get(url).pipe(fs.createWriteStream('index.html'))
+req.get(url).pipe(fs.createWriteStream(file_path))
