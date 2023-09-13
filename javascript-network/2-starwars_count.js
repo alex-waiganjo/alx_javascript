1894f1c5aca6fae66d990812bf6ecfc3dd0fdd78
alex-waiganjo <alexwaiganjo934@gmauil.com>
2023-09-14 01:00:15 +0300

    2

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,18 +1,17 @@
 #!/usr/bin/node
-
 const req = require('request');
-// Input ID
+
 const url = process.argv[2];
-const characterId =18
+const Id = 18;
 req.get(url, function (error, response, body) {
   const obj = JSON.parse(body).results;
-  const filmsWithWedgeAntilles = obj.filter((film) => {
-    const characterIds = film.characters.map((characterUrl) =>
-      parseInt(characterUrl.split('/').slice(-2, -1)[0]),
+  const films = obj.filter((film) => {
+    const char_Id = film.characters.map((Url) =>
+      parseInt(Url.split('/').slice(-2, -1)[0]),
     );
-    return characterIds.includes(characterId);
+    return char_Id.includes(Id);
   });
 
-  const count = filmsWithWedgeAntilles.length;
-  console.log(count);
+  const items = films.length;
+  console.log(items);
 });
